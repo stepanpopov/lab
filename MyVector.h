16f0b069e548143b3daf8553114c6f130c9bc30e
stepanpopov <stepan@DESKTOP-81FUGHF.localdomain>
2022-03-15 11:09:14 +0300

    work

--- MyVector.h
+++ MyVector.h
@@ -9,7 +9,14 @@ const int MAX_SIZE = 5;
 
 class MyVector {
 public:
-    MyVector(char *el = NULL, int maxsz = MAX_SIZE);
+    MyVector(char *el = NULL, int maxsz = MAX_SIZE) : maxsize(maxsz) {
+        pdata = new char * [maxsize];
+        pdata[0] = el;
+        for(int i = 1; i < maxsize; i++) {
+            pdata[i] = NULL;
+        }
+
+    }
 
     MyVector(MyVector &v);
 
