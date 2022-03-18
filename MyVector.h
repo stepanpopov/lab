88e065086e6639e15ba505206d639ed37676c11a
stepanpopov <stepan@DESKTOP-81FUGHF.localdomain>
2022-03-18 22:45:46 +0300

    workk

--- MyVector.h
+++ MyVector.h
@@ -2,6 +2,7 @@
 #define MYVECTOR_H
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -10,16 +11,18 @@ const int MAX_SIZE = 5;
 class MyVector {
 public:
     MyVector(char *el = NULL, int maxsz = MAX_SIZE) : maxsize(maxsz) {
-        pdata = new char * [maxsize];
+        pdata = new char *[maxsize];
         pdata[0] = el;
-        for(int i = 1; i < maxsize; i++) {
+        for (int i = 1; i < maxsize; i++) {
             pdata[i] = NULL;
         }
     }
 
     MyVector(MyVector &v);
 
-    ~MyVector();
+    ~MyVector() {
+        delete[] pdata;
+    }
 
     void add_element(char *el) {
         size++;
@@ -29,7 +32,15 @@ public:
         pdata[size - 1] = el;
     }
 
-    bool delete_element(int i);
+    bool delete_element(int i) {
+        for (int it = i; it < size - 1; ++it) {
+            pdata[it] = pdata[it + 1];
+        }
+        size--;
+        if (size < maxsize / 2) {
+            resize();
+        }
+    }
 
     char *operator[](int i) {
         if (i < 0 || i >= size) {
@@ -40,14 +51,29 @@ public:
     }
 
     void sort() {
-
+        char *temp;
+        for(int i = 0; i < size; ++i) {
+            for(int j = i + 1; j < size; ++j) {
+                if (strcmp(pdata[i],pdata[j]) > 0) {
+                    t = pdata[i];
+                    pdata[i] = pdata[j];
+                    pdata[j] = t;
+                }
+            }
+        }
     }
 
     int Size() { return size; }
 
     int Maxsize() { return maxsize; }
 
-    int find(char *el);
+    int find(char *el) {
+        for(int i = 0; i < size; ++i) {
+            if (strcmp(el, pdata[i]) == 0) {
+                return i;
+            }
+        }
+    }
 
     MyVector &operator=(MyVector &v);
 
@@ -61,7 +87,7 @@ private:
     void resize() {
         if (maxsize < size) {
             maxsize *= 1.5;
-            char ** newdata = new * char [maxsize];
+            char **newdata = new * char[maxsize];
             for (int i = 0; i < size; ++i) {
                 newdata[i] = pdata[i];
             }
@@ -70,8 +96,29 @@ private:
             }
             delete[] pdata;
             pdata = newdata;
+        } else if (size < maxsize / 2) {
+            maxsize /= 1.5;
         }
     }
+
+    /*int binarySearch(char* arr[], int x)
+    {
+        int l = 0;
+        int r = size - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (arr[m] == x)
+                return m;
+
+            if (arr[m] < x)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        return -1;
+    }*/
+
 };
 
 #endif
